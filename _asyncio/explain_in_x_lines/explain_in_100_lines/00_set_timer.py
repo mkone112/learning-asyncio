51b5ba4c4c279c2424c7c05a46fb1eeca58baaab
Maxim Kalita <kalitamaxwork@gmail.com>
2022-06-17 02:02:09 +0300

    добавил event_loop

--- _asyncio/explain_in_x_lines/explain_in_100_lines/00_set_timer.py
+++ _asyncio/explain_in_x_lines/explain_in_100_lines/00_set_timer.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 
 class _socket:
@@ -45,12 +46,55 @@ class Context:
 
 class set_timer(Context):
     """Convenience method to call event_loop.set_timer() without knowing about
-     the current event loop variable"""
+     the current event loop variable
+     регистрирует callback с задержкой в event loop
+     """
     def __init__(self, duration, callback):
         self.evloop.set_timer(duration, callback)
 
 
+class EventLoop:
+    def __init__(self):
+        self._queue = Queue()
+        self._time = None
+
+    def run(self, entry_point, *args):
+        self._execute(entry_point, *args)
+
+        while not self._queue.is_empty():
+            fn, mask = self._queue.pop(self._time)  # откуда берется self._time?
+            self._execute(fn, mask)
+
+        self._queue.close()
+
+    def _execute(self, callback, *args):
+        self._time = hrtime()
+        try:
+            callback(*args)
+        except Exception as err:
+            print('Uncaught exception:', err)
+        self._time = hrtime()
+
+    def register_fileobj(self, fileobj, callback):
+        """а нужно ли?"""
+        self._queue.register_fileobj(fileobj, callback)
+
+    def unregister_fileobj(self, fileobj):
+        """а нужно ли?"""
+        self._queue.unregister_fileobj(fileobj)
+
+    def set_timer(self, duration, callback):
+        self._time = hrtime()
+        # на данный момент lambda излишня
+        self._queue.register_timer(self._time + duration, lambda _: callback())
+
+
+def hrtime():
+    return int(time.time() * 10e6)
+
+
 def main():
+    # регистрируем event_loop._queue.register_fileobj(_socket, _socket.callbacks[...])
     sock = _socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def on_timer():
@@ -60,24 +104,26 @@ def main():
 
             def on_sent(err):
                 if err:
-                    sock.close()
+                    sock.close()  #?
                     raise err
 
                 def on_read(err, data=None):
-                    sock.close()
+                    sock.close()  #?
                     if err:
                         raise err
                     print(data)
 
                 sock.recv(1024, on_read)
 
-            sock.sendall(b'foobar', on_sent)
+            sock.sendall(b'foobar', on_sent)  #?
         # Регистрируем on_conn в _socket
         # и _sock.connect() должен быть запущен
         # начиная с этого момента и до завершения процедуры подключения - нам
         # нечего делать в скрипте -> event loop должен обрабатывать эту приостановку(?)
+        # пробрасывает это в
         sock.connect(('127.0.0.1', 53210), on_conn)
 
+    # is event_loop._queue.regiter_timer(hrtime() + 1000, on_timer)
     set_timer(1000, on_timer)
 
 
